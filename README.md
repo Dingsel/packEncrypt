3b3a9095e48d0782b5f70f310b91c1884a68bbc6
Dingsel <dshluka@gmail.com>
2024-01-04 14:37:15 +0100

    init

--- README.md
+++ README.md
@@ -1,2 +1,29 @@
-# packEncrypt
-This Is A Regolith Filter To Automaticly Encrypt RP Packs Using A Build Tool Called Regolith. This Regolith Filter Can Be Modified To Work In Other Enviroments Aswell.
+# Pack Encrypt
+
+Pack Encrypt Is A [Regolith Filter](https://bedrock-oss.github.io/regolith/guide/filters) To Automaticly Encrypt RP Packs Using A Build Tool Called Regolith. This [Regolith Filter](https://bedrock-oss.github.io/regolith/guide/filters) Can Be Modified To Work In Other Enviroments Aswell.
+
+## How It Works
+⚠️NOT EVERY SERVER SOFTWARE SUPPORTS ENCRYPTED PACKS AND I AM UNAWARE OF METHODS TO MAKE IT WORK IN WORLDS⚠️
+
+After Runing The Filer The Decryption Key Will Be Displayed In The Console 
+![Getting The Encryption Key](images/image.png)
+
+Now The Pack Contents Have To Be Zipped Manually.
+![Zipping The Output](images/image-2.png)
+
+Now Create A .key File With The Same Name As The Zipped Folder And Put Your Key Inside The File
+![How To Use](images/image-3.png)
+
+🎉And Now You Are Ready To Use The Encrypted Pack.
+
+NOTE : ONLY SOME SERVER SOFTWARES SUPPORT ENCRYPTED PACKS
+BDS IS NOT ONE OF THEM
+
+| Server Software | Supported Status |
+| ----------- | ----------- |
+| Nukkit | ✔️ |
+| PoketMine | ✔️ |
+| WaterDogPE Proxy | ✔️ |
+| BDS | ⭕ |
+| BDSX | Need To Test But Probably Not |
+| LiteLoaderBDS / LeviLamina | Need To Test But Probably Not |
---
+++ index.js
@@ -0,0 +1,49 @@
+import { existsSync, readFileSync, writeFileSync } from 'fs';
+// @ts-ignore
+import pkg from 'aes-js';
+const { ModeOfOperation, utils } = pkg;
+import { glob } from 'glob';
+
+const exclude = ["manifest.json", "pack_icon.png"]
+
+const keyBuffer = Buffer.from(Array.from({ length: 32 }, () => String.fromCharCode(Math.floor(Math.random() * 26) + 97)).join(''));
+const mainKey = new Uint8Array(keyBuffer);
+const iv = mainKey.slice(0, 16)
+
+if (!existsSync("./RP/manifest.json")) throw new TypeError("No Manifest Or Invalid File Structure")
+
+const uuid = JSON.parse(readFileSync("./RP/manifest.json").toString())["header"]["uuid"];
+if (!uuid) throw new TypeError("Invalid Manifest Or No UUID")
+
+const contentsJSON = {
+    content: []
+}
+
+const files = glob.sync("./RP/**/*", { nodir: true });
+for (const file of files) {
+    const fileBuffer = readFileSync(file)
+    if (exclude.some(x => file.endsWith(x))) { writeFileSync(file, fileBuffer); continue }
+    const byteArr = Uint8Array.from(fileBuffer)
+    const subKey = new Uint8Array(Buffer.from(Array.from({ length: 32 }, () => String.fromCharCode(Math.floor(Math.random() * 26) + 97)).join('')))
+    const mode = new ModeOfOperation.cfb(subKey, subKey.slice(0, 16), 1)
+    const arr = mode.encrypt(byteArr)
+    writeFileSync(file, arr)
+    contentsJSON.content.push({
+        key: Buffer.from(subKey.buffer).toString("utf-8"),
+        path: "." + file.split("RP", 2)[1].replace(/\\/g, "/")
+    })
+}
+const versionBuffer = Buffer.from([0x00, 0x00, 0x00, 0x00]);
+const magicBuffer = Buffer.from([0xFC, 0xB9, 0xCF, 0x9B]);
+const idBytes = Buffer.from([uuid.length]);
+
+const header = Buffer.concat([versionBuffer, magicBuffer, Buffer.alloc(0x08), idBytes, Buffer.from(uuid), Buffer.alloc(0xcb)]);
+
+const buffer = Buffer.from(JSON.stringify(contentsJSON))
+
+const cipher = new ModeOfOperation.cfb(mainKey, iv, 1)
+
+const file = Buffer.concat([header, Buffer.from(cipher.encrypt(buffer).buffer)])
+console.warn(Buffer.from(mainKey).toString("utf-8"))
+
+writeFileSync("./RP/contents.json", file)
