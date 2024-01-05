00b4c3fd5dc3a620cdf979537d87afc6662f8bc8
Dingsel <dshluka@gmail.com>
2024-01-05 16:55:03 +0100

    Manifest Is Not Included In contents.json

--- packEncrypt/index.js
+++ packEncrypt/index.js
@@ -1,7 +1,7 @@
 import { existsSync, readFileSync, writeFileSync } from 'fs';
 // @ts-ignore
 import pkg from 'aes-js';
-const { ModeOfOperation, utils } = pkg;
+const { ModeOfOperation } = pkg;
 import { glob } from 'glob';
 
 const exclude = ["manifest.json", "pack_icon.png"]
@@ -22,7 +22,12 @@ const contentsJSON = {
 const files = glob.sync("./RP/**/*", { nodir: true });
 for (const file of files) {
     const fileBuffer = readFileSync(file)
-    if (exclude.some(x => file.endsWith(x))) { writeFileSync(file, fileBuffer); continue }
+    if (exclude.some(x => file.endsWith(x))) {
+        writeFileSync(file, fileBuffer); contentsJSON.content.push({
+            path: "." + file.split("RP", 2)[1].replace(/\\/g, "/")
+        });
+        continue
+    }
     const byteArr = Uint8Array.from(fileBuffer)
     const subKey = new Uint8Array(Buffer.from(Array.from({ length: 32 }, () => String.fromCharCode(Math.floor(Math.random() * 26) + 97)).join('')))
     const mode = new ModeOfOperation.cfb(subKey, subKey.slice(0, 16), 1)
