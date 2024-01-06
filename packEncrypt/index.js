94155d10707bf6017a163ba51afcb13ebb7378f4
Dingsel <dshluka@gmail.com>
2024-01-06 16:28:14 +0100

    Fixed Paths + manifest key as null

--- packEncrypt/index.js
+++ packEncrypt/index.js
@@ -1,14 +1,18 @@
 import { existsSync, readFileSync, writeFileSync } from 'fs';
 // @ts-ignore
 import pkg from 'aes-js';
-const { ModeOfOperation } = pkg;
+const { ModeOfOperation, utils } = pkg;
 import { glob } from 'glob';
 
 const exclude = ["manifest.json", "pack_icon.png"]
 
-const keyBuffer = Buffer.from(Array.from({ length: 32 }, () => String.fromCharCode(Math.floor(Math.random() * 26) + 97)).join(''));
-const mainKey = new Uint8Array(keyBuffer);
-const iv = mainKey.slice(0, 16)
+const mainKey = Buffer.from(
+    Array.from({ length: 32 }, () => {
+        const characters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
+        return characters.charAt(Math.floor(Math.random() * characters.length));
+    }).join('')
+)
+const iv = mainKey.subarray(0, 16)
 
 if (!existsSync("./RP/manifest.json")) throw new TypeError("No Manifest Or Invalid File Structure")
 
@@ -24,18 +28,23 @@ for (const file of files) {
     const fileBuffer = readFileSync(file)
     if (exclude.some(x => file.endsWith(x))) {
         writeFileSync(file, fileBuffer); contentsJSON.content.push({
-            path: "." + file.split("RP", 2)[1].replace(/\\/g, "/")
+            path: "." + file.split("RP", 2)[1].replace(/\\/g, "/"),
+            key: null
         });
         continue
     }
-    const byteArr = Uint8Array.from(fileBuffer)
-    const subKey = new Uint8Array(Buffer.from(Array.from({ length: 32 }, () => String.fromCharCode(Math.floor(Math.random() * 26) + 97)).join('')))
-    const mode = new ModeOfOperation.cfb(subKey, subKey.slice(0, 16), 1)
-    const arr = mode.encrypt(byteArr)
+    const subKey = Buffer.from(
+        Array.from({ length: 32 }, () => {
+            const characters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
+            return characters.charAt(Math.floor(Math.random() * characters.length));
+        }).join('')
+    )
+    const mode = new ModeOfOperation.cfb(subKey, subKey.subarray(0, 16), 1)
+    const arr = mode.encrypt(fileBuffer)
     writeFileSync(file, arr)
     contentsJSON.content.push({
-        key: Buffer.from(subKey.buffer).toString("utf-8"),
-        path: "." + file.split("RP", 2)[1].replace(/\\/g, "/")
+        key: subKey.toString("utf-8"),
+        path: file.split("RP", 2)[1].replace(/\\/g, "/").substring(1)
     })
 }
 const versionBuffer = Buffer.from([0x00, 0x00, 0x00, 0x00]);
@@ -49,6 +58,6 @@ const buffer = Buffer.from(JSON.stringify(contentsJSON))
 const cipher = new ModeOfOperation.cfb(mainKey, iv, 1)
 
 const file = Buffer.concat([header, Buffer.from(cipher.encrypt(buffer).buffer)])
-console.warn(Buffer.from(mainKey).toString("utf-8"))
+console.warn(mainKey.toString())
 
 writeFileSync("./RP/contents.json", file)
